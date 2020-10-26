2dfcaa76c169e5d08435347f528566e61112da94
Jennifer Johnson <jennifer@storj.io>
2020-10-26 14:02:50 -0400

    update parse records test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -8,12 +8,29 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"storj.io/common/testcontext"
+	"storj.io/storj/private/testplanet"
 )
 
 func TestParseRecords(t *testing.T) {
-	records := []string{"storj_grant-2:grant2", "storj_root:linkshare/test", "storj_grant-1:grant1"}
-	serializedAccess, root, err := parseRecords(records)
-	require.NoError(t, err)
-	assert.Equal(t, "grant1grant2", serializedAccess)
-	assert.Equal(t, "linkshare/test", root)
+	ctx := testcontext.New(t)
+	defer ctx.Cleanup()
+	testplanet.Run(t, testplanet.Config{
+		SatelliteCount:   1,
+		StorageNodeCount: 1,
+		UplinkCount:      1,
+	}, func(t *testing.T, ctx *testcontext.Context, planet *testplanet.Planet) {
+		access := planet.Uplinks[0].Access[planet.Satellites[0].ID()]
+		serializedAccess, err := access.Serialize()
+		require.NoError(t, err)
+		midpoint := len(serializedAccess) / 2
+		r1 := serializedAccess[:midpoint]
+		r2 := serializedAccess[midpoint:]
+		records := []string{"storj_grant-2:" + r2, "storj_root:linkshare/test", "storj_grant-1:" + r1}
+		parsedAccess, root, err := parseRecords(records)
+		require.NoError(t, err)
+		assert.Equal(t, access, parsedAccess)
+		assert.Equal(t, "linkshare/test", root)
+	})
 }
